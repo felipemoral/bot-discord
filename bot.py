713ce16887a3e94e606d86c8ff58ad05297208b7
felipemoral <140470328+felipemoral@users.noreply.github.com>
2026-02-15 14:01:38 -0400

    bot.py

--- bot.py
+++ bot.py
@@ -1,50 +1,18 @@
-import os
 import discord
 from discord.ext import commands
-import requests
-
-@bot.event
-async def on_message(message):
-    print(message.content)
-    await bot.process_commands(message)
-
-
-
-TOKEN = os.getenv("DISCORD_TOKEN")
-API_URL = "https://metaforge.app/api/arc-raiders/items"
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-items_cache = []
-
 @bot.event
 async def on_ready():
-    print(f"Bot online como {bot.user}")
-    try:
-        r = requests.get(API_URL)
-        global items_cache
-        items_cache = r.json()
-        print("Itens carregados!")
-    except:
-        print("Erro ao carregar itens.")
+    print(f"Logado como {bot.user}")
 
 @bot.command()
 async def itens(ctx):
-    await ctx.send("Comando funcionando ✅")
+    await ctx.send("FUNCIONANDO ✅")
 
-    if not items_cache:
-        return await ctx.send("Erro ao carregar itens.")
-    
-    mensagem = "📦 Lista de Itens:\n"
-    for item in items_cache[:20]:
-        mensagem += f"- {item.get('name')} ({item.get('rarity')})\n"
-    
-    await ctx.send(mensagem)
-
-bot.run("MTQ2OTA5MDA5OTE5Mzk3NDk2NQ.GJ9Qoa.vF9qHXIkq0UDXKNi5PZsBWTK01AHGcNyn53Lhc
-")
-
+bot.run(os.getenv("TOKEN"))
