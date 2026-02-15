2a83699b672e551ed2efd6371aa06793fd8a34f0
felipemoral <140470328+felipemoral@users.noreply.github.com>
2026-02-15 14:13:25 -0400

    bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,24 @@ import discord
 from discord.ext import commands
 import os
 
-# 1️⃣ Criar intents
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-# 2️⃣ Criar o bot
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# 3️⃣ Eventos
+
 @bot.event
 async def on_ready():
     print(f"Logado como {bot.user}")
 
-# 4️⃣ Comandos
 @bot.command()
 async def itens(ctx):
     await ctx.send("FUNCIONANDO ✅")
 
-# 5️⃣ Rodar o bot
+print("TOKEN:", os.getenv("TOKEN"))
+
+
 bot.run(os.getenv("MTQ2OTA5MDA5OTE5Mzk3NDk2NQ.GJ9Qoa.vF9qHXIkq0UDXKNi5PZsBWTK01AHGcNyn53Lhc
 "))
