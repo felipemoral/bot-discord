5e67623bcd6ee43f62e165043052e651829ebf2e
felipemoral <140470328+felipemoral@users.noreply.github.com>
2026-02-15 14:19:50 -0400

    bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 @bot.event
 async def on_ready():
